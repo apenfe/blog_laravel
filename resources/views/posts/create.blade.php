ae2ef741db67fc28f0f24ba888e11b2dbc9e7978
Adrián Peñalver Fernández <adrian1414@hotmail.es>
2024-10-11 15:56:57 +0200

    a medio paso 14

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,18 +2,30 @@
 
     <h1>Create a new post</h1>
 
+    @foreach($errors->all() as $error)
+        <p>{{ $error }}</p>
+    @endforeach
+
     <form method="POST" action="{{ route('posts.store') }}">
         @csrf
 
         <label>
             Title <br>
             <input type="text" name="title" placeholder="Inserte un titulo">
+
+            @error('title')
+                <p><strong style="color: red">{{ $message }}</strong></p>
+            @enderror
         </label>
         <br>
 
         <label>
             Body <br>
             <textarea name="body" placeholder="Inserte el body del post"></textarea>
+
+            @error('body')
+                <p><strong style="color: red">{{ $message }}</strong></p>
+            @enderror
         </label>
         <br>
 
