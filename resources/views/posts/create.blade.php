f572155f977d7aa20aaa41d33ea900dcc79390fc
Adrián Peñalver Fernández <adrian1414@hotmail.es>
2024-10-15 16:13:28 +0200

    FIN PASO 18 - refactorizacion formularios

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,27 +5,7 @@
     <form method="POST" action="{{ route('posts.store') }}">
         @csrf
 
-        <label>
-            {{ __('Title') }}
-           <br>
-            <input type="text" name="title" placeholder="Inserte un titulo" value="{{ old('title') }}">
-
-            @error('title')
-                <p><small style="color: red">{{ $message }}</small></p>
-            @enderror
-        </label>
-        <br>
-
-        <label>
-            {{ __('Body') }}
-             <br>
-            <textarea name="body" placeholder="Inserte el body del post">{{ old('body') }}</textarea>
-
-            @error('body')
-                <p><small style="color: red">{{ $message }}</small></p>
-            @enderror
-        </label>
-        <br>
+        @include('posts.form-fields')
 
         <input type="submit" value="{{ __('Submit') }}">
         <br>
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -8,27 +8,7 @@
         @csrf
         @method('PATCH')
 
-        <label>
-            {{ __('Title') }}
-            <br>
-            <input type="text" name="title" placeholder="Inserte un titulo" value="{{ old('title', $post->title) }}">
-
-            @error('title')
-            <p><small style="color: red">{{ $message }}</small></p>
-            @enderror
-        </label>
-        <br>
-
-        <label>
-            {{ __('Body') }}
-            <br>
-            <textarea name="body" placeholder="Inserte el body del post">{{ old('body', $post->body) }}</textarea>
-
-            @error('body')
-            <p><small style="color: red">{{ $message }}</small></p>
-            @enderror
-        </label>
-        <br>
+        @include('posts.form-fields')
 
         <input type="submit" value="{{ __('Submit') }}">
         <br>
