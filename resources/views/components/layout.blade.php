9c3b0e2c17735c623104c23833f59fc2bf7a0388
Adrián Peñalver Fernández <adrian1414@hotmail.es>
2024-10-16 17:12:35 +0200

    mitad paso 23

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,7 +10,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
-<body class="font-sans antialiased dark:bg-black dark:text-white/50">
+<body class="flex h-screen flex-col bg-slate-100 selection:bg-sky-600 selection:text-sky-50 dark:bg-slate-950">
 
 <x-partials.navigation />
 
@@ -20,14 +20,9 @@
 </div>
 @endsession
 
-{{ $slot }} {{-- This is where the content will be injected ANONIMO --}}
-
-@isset($sidebar)
-    <div id="sidebar">
-        <h3>Sidebar</h3>
-        {{ $sidebar }}
-    </div>
-@endisset
+<main class="flex-1 p-4">
+    {{ $slot }} {{-- This is where the content will be injected ANONIMO --}}
+</main>
 
 </body>
 </html>
