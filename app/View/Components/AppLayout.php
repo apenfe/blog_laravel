f918fb144668139201c4edea17745096c74e44f6
Adrián Peñalver Fernández <adrian1414@hotmail.es>
2024-10-18 16:29:23 +0200

    Fin paso 26

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -7,6 +7,14 @@ use Illuminate\View\View;
 
 class AppLayout extends Component
 {
+
+    public function __construct(
+       public string $metaTitle = 'default title',
+       public string $metaDescription = 'default description',
+    ) {
+
+    }
+
     /**
      * Get the view / contents that represents the component.
      */
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,20 +1,26 @@
-<x-layout meta-title="Create a new post" meta-description="Form to Create a new post">
+<x-app-layout meta-title="Create a new post" meta-description="Form to Create a new post">
 
-    <h1>{{ __('Create a new post') }}</h1>
-    <a href="{{ route('posts.index') }}">{{ __('Back') }}</a>
-    <hr>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Create a new post') }}
+        </h2>
+    </x-slot>
 
-    <form method="POST" action="{{ route('posts.store') }}">
-        @csrf
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <form method="POST" action="{{ route('posts.store') }}">
+                        @csrf
 
-        @include('posts.form-fields')
+                        @include('posts.form-fields')
 
-        <input type="submit" value="{{ __('Submit') }}">
-        <br>
+                        <input type="submit" value="{{ __('Submit') }}">
 
-    </form>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 
-    <hr>
-    <a href="{{ route('posts.index') }}">{{ __('Back') }}</a>
-
-</x-layout>
+</x-app-layout>
