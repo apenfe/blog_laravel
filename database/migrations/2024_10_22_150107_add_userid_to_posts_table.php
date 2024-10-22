d0728d8237cd604b0704cfbc8bf7f0b78fbf57b6
Adrián Peñalver Fernández <adrian1414@hotmail.es>
2024-10-22 17:26:03 +0200

    3 tarea, falta que se añada id en creacion y el published at automatico

---
+++ database/migrations/2024_10_22_150107_add_userid_to_posts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+
+            // añadir una columna userid a la tabla post asociada a la tabla users
+            $table->foreignIdFor(User::class);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
+        });
+    }
+};
