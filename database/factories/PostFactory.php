63dddd73a423512455ba3dc2c6824aff6f96b888
Adrián Peñalver Fernández <adrian1414@hotmail.es>
2024-10-23 16:55:09 +0200

    creadas las factorias y los seeders

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    protected $model = Post::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'body' => $this->faker->paragraph,
+            'user_id' => User::factory(),
+            'published_at' => $this->faker->dateTime,
+        ];
+    }
+}
